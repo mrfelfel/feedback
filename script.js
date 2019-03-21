8536329cbc4fdc2753aa5d1de47ed2a188e9f7dd
mrfelfel <qazvinyjavad@gmail.com>
2019-03-21 14:44:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -336,6 +336,9 @@ div#feedback.message div.input{
 }
 
 function ShowFeedBack(){
+    loveIndex = 0;
+    document.querySelectorAll("div#feedback div.emojis div.emoji").forEach(emoji=>emoji.classList.remove('focus'))
+    document.getElementById('feedback').classList.remove('message')
     document.getElementById('feedback').classList.add('enable');
     setTimeout(() => {
         document.getElementById('feedback').classList.add('content');
